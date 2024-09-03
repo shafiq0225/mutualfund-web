aa47facdebe21a5d780479b44461addf0a2d14bd
shafiq0225 <shafiqahamed.be@gmail.com>
2024-09-03 22:00:22 +0530

    Add nav history component

---
+++ src/app/nav-history/nav-history.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-history',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-history.component.html',
+  styleUrl: './nav-history.component.css'
+})
+export class NavHistoryComponent {
+
+}
