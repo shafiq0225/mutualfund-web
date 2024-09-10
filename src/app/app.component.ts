8f4d2acde401e24b0da9aefa3b31c78f146a5111
shafiq0225 <shafiqahamed.be@gmail.com>
2024-09-10 10:43:00 +0530

    version 17 installed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
-import { FullComponent } from "./layout/full/full.component";
-import { TablerIconsModule } from 'angular-tabler-icons';
-import * as TablerIcons from 'angular-tabler-icons/icons';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [FullComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
